efc008db7ee4f46d98199907498a2be7563ffc99
iamaamir <8420386+iamaamir@users.noreply.github.com>
2026-05-31 13:02:58 +0530

    chore: skip resore mock

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     environment: "node",
     include: ["tests/**/*.test.js"],
-    restoreMocks: true,
+    restoreMocks: false,
     clearMocks: true,
     mockReset: true,
   },
